5d99f9d0c30fbaf209702764554cc9e7d5895239
worknovel <ahmadhasnol@hotmail.com>
2022-03-27 21:40:29 +0800

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useSpring, animated, config } from "@react-spring/three";
+
+import "./App.scss";
+import { MeshWobbleMaterial } from "@react-three/drei";
+
+const Shape = ({ position, color }) => {
+  const [active, setActive] = useState(false);
+  const { scale } = useSpring({
+    scale: active ? 1.5 : 1,
+    config: config.wobbly,
+  });
+  const shapeRef = useRef(null);
+
+  useFrame(({ clock }) => {
+    shapeRef.current.rotation.x = Math.sin(clock.getElapsedTime());
+  });
+
+  return (
+    <>
+      <animated.mesh
+        position={position}
+        scale={scale}
+        castShadow
+        onClick={() => setActive(!active)}
+        ref={shapeRef}
+      >
+        <boxGeometry />
+        <MeshWobbleMaterial color={color} />
+      </animated.mesh>
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="canvas-container">
+        <Canvas shadows>
+          <ambientLight intensity={0.3} />
+          <directionalLight
+            color="white"
+            position={[0, 10, 0]}
+            castShadow
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
+          <group>
+            <mesh
+              rotation={[-0.5 * Math.PI, 0, 0]}
+              position={[0, -1, 0]}
+              receiveShadow
+            >
+              <planeBufferGeometry args={[10, 10, 1, 1]} />
+              <shadowMaterial transparent opacity={0.2} />
+            </mesh>
+            <Shape color="pink" position={[-2, 0, 0]} />
+            <Shape color="lightblue" position={[2, 0, 0]} />
+          </group>
+        </Canvas>
+      </div>
+      <h4 className="title">&quot;Experimental&quot;</h4>
+    </>
   );
 }
 
