403956245816f9a363896ccd5f723e95aa76f63b
worknovel <ahmadhasnol@hotmail.com>
2022-03-27 22:11:34 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useRef, useState, Suspense } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { useSpring, animated, config } from "@react-spring/three";
+import { MeshWobbleMaterial } from "@react-three/drei";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 
 import "./App.scss";
-import { MeshWobbleMaterial } from "@react-three/drei";
 
-const Shape = ({ position, color }) => {
+const textureDark =
+  "https://images.unsplash.com/photo-1630182048152-ed2ea240b6cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80";
+const textureLight =
+  "https://images.unsplash.com/photo-1648318579308-d5ddaa38c4da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80";
+
+const Shape = ({ position, texture }) => {
   const [active, setActive] = useState(false);
   const { scale } = useSpring({
     scale: active ? 1.5 : 1,
     config: config.wobbly,
   });
   const shapeRef = useRef(null);
+  const colorMap = useLoader(TextureLoader, texture);
 
   useFrame(({ clock }) => {
     shapeRef.current.rotation.x = Math.sin(clock.getElapsedTime());
@@ -27,7 +34,7 @@ const Shape = ({ position, color }) => {
         ref={shapeRef}
       >
         <boxGeometry />
-        <MeshWobbleMaterial color={color} />
+        <MeshWobbleMaterial map={colorMap} />
       </animated.mesh>
     </>
   );
@@ -38,26 +45,28 @@ function App() {
     <>
       <div className="canvas-container">
         <Canvas shadows>
-          <ambientLight intensity={0.3} />
-          <directionalLight
-            color="white"
-            position={[0, 10, 0]}
-            castShadow
-            shadow-mapSize-width={1024}
-            shadow-mapSize-height={1024}
-          />
-          <group>
-            <mesh
-              rotation={[-0.5 * Math.PI, 0, 0]}
-              position={[0, -1, 0]}
-              receiveShadow
-            >
-              <planeBufferGeometry args={[10, 10, 1, 1]} />
-              <shadowMaterial transparent opacity={0.2} />
-            </mesh>
-            <Shape color="pink" position={[-2, 0, 0]} />
-            <Shape color="lightblue" position={[2, 0, 0]} />
-          </group>
+          <Suspense fallback={null}>
+            <ambientLight intensity={0.5} />
+            <directionalLight
+              color="white"
+              position={[0, 10, 0]}
+              castShadow
+              shadow-mapSize-width={1024}
+              shadow-mapSize-height={1024}
+            />
+            <group>
+              <mesh
+                rotation={[-0.5 * Math.PI, 0, 0]}
+                position={[0, -1, 0]}
+                receiveShadow
+              >
+                <planeBufferGeometry args={[10, 10, 1, 1]} />
+                <shadowMaterial transparent opacity={0.2} />
+              </mesh>
+              <Shape texture={textureLight} position={[-1.5, 0, 0]} />
+              <Shape texture={textureDark} position={[1.5, 0, 0]} />
+            </group>
+          </Suspense>
         </Canvas>
       </div>
       <h4 className="title">&quot;Experimental&quot;</h4>
